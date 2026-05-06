72a7f5dacf3b002adeaffd1fa1ec26fd370dffc2
agnes77horvath_glitch <agnes77.horvath@gmail.com>
2026-05-06 17:48:55 +0200

    javitas

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -31,7 +31,7 @@ class TestHootel(object):
     @allure.tag("login")
     def test_login(self):
         #login_btn = self.browser.find_element(By.XPATH, '//a[@class="nav-link"]')
-        login_btn = WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable(By.XPATH, '//a[@class="nav-link"]'))
+        login_btn = WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
         login_btn.click()
 
         email_input = self.browser.find_element(By.ID, 'email')
